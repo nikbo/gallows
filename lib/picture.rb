20f3574c02496935ebe165f9602025ab3830a4a4
nikbo <nikita-bobrik@yandex.ru>
2013-03-11 22:04:14 +0300

    Fixed picture

--- lib/picture.rb
+++ lib/picture.rb
@@ -1,27 +1,33 @@
 class Picture
+  attr_accessor :head_1, :head_2, :head_3, :head_4, :head_5, :hand_1, :hand_2, :body_1, :body_2, :left_foot_1, :left_foot_2, :right_foot_1, :right_foot_2
   def initialize
-     @head="/ \\               |
-    \\ /"
-    @neck=" |"
-    @arm_1="---"
-    @arm_2="---"
-    @body="|                |
-     |"
-     @leg_2="\\"
-    @leg_1="/"
-
+    @head_1="/"
+    @head_2="\\"
+    @head_3="\\"
+    @head_4="/"
+    @head_5="|"
+    @hand_1="---"
+    @hand_2="----"
+    @body_1="|"
+    @body_2="|"
+    @left_foot_1="/"
+    @left_foot_2="/"
+    @right_foot_1="\\"
+    @right_foot_2="\\"
   end
   def pic
-     puts "
-      ________________
-    #{@head}               |
-    #{@neck}                |
-  #{@arm_1}#{@arm_2}              |
-     #{@body}                |
-    #{@leg_1}#{@leg_2}                |
-                      |
-                ______|_____
-
+    puts "
+    ________________
+   #{@head_1} #{@head_2}              |
+   #{@head_3} #{@head_4}              |
+    #{@head_5}               |
+ #{@hand_1}#{@hand_2}            |
+    #{@body_1}               |
+    #{@body_2}               |
+   #{@left_foot_1}#{@right_foot_1}               |
+  #{@left_foot_2}  #{@right_foot_2}              |
+              ______|_____
 "
+
   end
 end
---
+++ lib/words.rb
@@ -0,0 +1,9 @@
+class Words
+  def initialize
+     @word={1=>"paper", 2=>"mouse", 3=>"computer", 4=>"cat", 5=>"window", 6=>"apple", 7=>"water", 8=>"phone",
+     9=>"air"}
+  end
+  def random
+    rand(1..9)
+  end
+end
